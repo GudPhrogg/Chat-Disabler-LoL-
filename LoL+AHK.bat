75443cd264788ddfe5ea23b8c6f8de34567d2493
GudPhrogg <102308375+GudPhrogg@users.noreply.github.com>
2022-03-24 16:01:14 +0100

    Add files via upload

---
+++ LoL+AHK.bat
@@ -0,0 +1,5 @@
+@echo off 
+
+START Chat_Disabler_(LoL).ahk "‪CHANGE LOCATION HERE"
+
+START "product=league_of_legends --launch-patchline=live" "CHANGE LOCATION HERE" --launch-product=league_of_legends --launch-patchline=live
