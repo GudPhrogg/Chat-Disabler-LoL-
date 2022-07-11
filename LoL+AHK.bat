26ebe96e307c1b3ff8d6542c5c7dfe662902764a
GudPhrogg <102308375+GudPhrogg@users.noreply.github.com>
2022-07-11 20:47:14 +0200

    Update LoL+AHK.bat

--- LoL+AHK.bat
+++ LoL+AHK.bat
@@ -1,5 +1,5 @@
 @echo off 
 
-START Chat_Disable.ahk "‪CHANGE LOCATION HERE"
+START "Disable enter LoL.exe" "‪CHANGE LOCATION HERE"
 
 START "product=league_of_legends --launch-patchline=live" "CHANGE LOCATION HERE" --launch-product=league_of_legends --launch-patchline=live
